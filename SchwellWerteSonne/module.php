479dc58e1e4e1f43926f05f3de0d8468c7be0f9a
DonCri <36209368+DonCri@users.noreply.github.com>
2018-02-10 18:31:14 +0100

    Update module.php

--- SchwellWerteSonne/module.php
+++ SchwellWerteSonne/module.php
@@ -1,6 +1,34 @@
 <?
     // Klassendefinition
     class SchwellWerteSonneSoll extends IPSModule {
+
+        // Der Konstruktor des Moduls
+        // Überschreibt den Standard Kontruktor von IPS
+        public function __construct($InstanceID) {
+            // Diese Zeile nicht löschen
+            parent::__construct($InstanceID);
+
+            // Selbsterstellter Code
+        }
+
+        // Überschreibt die interne IPS_Create($id) Funktion
+        public function Create() {
+            // Diese Zeile nicht löschen.
+            parent::Create();
+
+            $this->RegisterPropertyString("Benutzername", "MaxMustermann");
+            $this->RegisterPropertyInteger("Zahl", 123);
+            $this->RegisterPropertyFloat("Faktor", 0.5);
+            $this->RegisterPropertyBoolean("Geöffnet", true);
+
+        }
+
+        // Überschreibt die intere IPS_ApplyChanges($id) Funktion
+        public function ApplyChanges() {
+            // Diese Zeile nicht löschen
+            parent::ApplyChanges();
+        }
+
         /**
         * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
         * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wiefolgt zur Verfügung gestellt:
@@ -8,14 +36,8 @@
         * ABC_MeineErsteEigeneFunktion($id);
         *
         */
-        public function Create() {
-          parent::Create();
-
-// Modul-Eigenschaftserstellung
-$this->RegisterPropertyString("Benutzername", "MaxMustermann");
-$this->RegisterPropertyInteger("Zahl", 123);
-$this->RegisterPropertyFloat("Faktor", 0.5);
-$this->RegisterPropertyBoolean("Geöffnet", true);
+        public function MeineErsteEigeneFunktion() {
+            // Selbsterstellter Code
         }
     }
 ?>
