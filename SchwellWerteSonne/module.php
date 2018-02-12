cccfce203f6b35578f8a891618e5f525f514c034
DonCri <36209368+DonCri@users.noreply.github.com>
2018-02-12 23:09:52 +0100

    Add files via upload

--- SchwellWerteSonne/module.php
+++ SchwellWerteSonne/module.php
@@ -1,7 +1,7 @@
 <?
 
     // Klassendefinition
-    class SchwellWerteSonneSoll extends IPSModule {
+    class SchwellWerteSWW extends IPSModule {
         /**
         * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
         * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wiefolgt zur Verfügung gestellt:
